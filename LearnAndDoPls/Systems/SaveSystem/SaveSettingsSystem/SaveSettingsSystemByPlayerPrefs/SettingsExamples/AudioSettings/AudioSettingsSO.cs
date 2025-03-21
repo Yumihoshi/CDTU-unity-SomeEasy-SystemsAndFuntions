334a3b9b7404298766779bf05d3396751aede338
whatevertogo <1879483647@qq.com>
2025-03-22 04:34:19 +0800

    优化AudioSettingsSO类的代码结构，移除冗余命名空间声明

--- LearnAndDoPls/Systems/SaveSystem/SaveSettingsSystem/SaveSettingsSystemByPlayerPrefs/SettingsExamples/AudioSettings/AudioSettingsSO.cs
+++ LearnAndDoPls/Systems/SaveSystem/SaveSettingsSystem/SaveSettingsSystemByPlayerPrefs/SettingsExamples/AudioSettings/AudioSettingsSO.cs
@@ -1,18 +1,15 @@
 using UnityEngine;
 
-namespace SaveSystem
+
+[CreateAssetMenu(fileName = "AudioSettingsSO", menuName = "Settings/Audio SettingsSO")]
+public class AudioSettingsSO : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "AudioSettingsSO", menuName = "Settings/Audio SettingsSO")]
-    public class AudioSettingsSO : ScriptableObject
-    {
-        [Header("音量数据")]
-        [SerializeField, Range(0, 1), Tooltip("主音量")]
-        public float masterVolume = 1f;
-        
-        [SerializeField, Range(0, 1), Tooltip("背景音乐音量")]
-        public float bgmVolume = 1f;
-        
-        [SerializeField, Range(0, 1), Tooltip("音效音量")]
-        public float sfxVolume = 1f;
-    }
+    [Header("音量数据")] [SerializeField, Range(0, 1), Tooltip("主音量")]
+    public float masterVolume = 1f;
+
+    [SerializeField, Range(0, 1), Tooltip("背景音乐音量")]
+    public float bgmVolume = 1f;
+
+    [SerializeField, Range(0, 1), Tooltip("音效音量")]
+    public float sfxVolume = 1f;
 }
